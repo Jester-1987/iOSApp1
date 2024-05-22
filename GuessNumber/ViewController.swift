3031d0237663c2cf90465b54eb36723357f1186e
Jester-1987 <nicole.groeger@triosstudent.com>
2024-05-22 13:16:14 -0400

    fixed slider display

--- GuessNumber/ViewController.swift
+++ GuessNumber/ViewController.swift
@@ -1,19 +1,46 @@
-//
-//  ViewController.swift
-//  GuessNumber
-//
-//  Created by Nicole Groeger on 2024-05-22.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+  
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var sLabel: UILabel!
+    
+    
+    var currentValue = 0
+    var targetValue = 0
+    var round = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startNewRound()
     }
-
-
+    
+    @IBAction func showAlert() {
+       
+        let alert = UIAlertController(
+            title: "Your Guess",
+            message: "You guessed: ",
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(
+            title: "OK",
+            style: .default,
+            handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider){
+        let currentValue = Int(slider.value)
+        sLabel.text = "\(currentValue)"
+            }
+    
+    func startNewRound() {
+        round += 1
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+    }
 }
-
