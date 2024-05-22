7896958220bc39b6c35af7654c1dbbdf717daff5
Jester-1987 <nicole.groeger@triosstudent.com>
2024-05-22 13:40:45 -0400

    fixed bug with slider

--- GuessNumber/ViewController.swift
+++ GuessNumber/ViewController.swift
@@ -13,34 +13,39 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        currentValue = lroundf(slider.value)
+        targetValue = Int.random(in: 1...100)
         startNewRound()
     }
     
     @IBAction func showAlert() {
        
         let alert = UIAlertController(
-            title: "Your Guess",
-            message: "You guessed: ",
+            title: "\(targetValue)",
+            message: "How close were you?",
             preferredStyle: .alert)
         
         let action = UIAlertAction(
-            title: "OK",
+            title: "Try again!",
             style: .default,
             handler: nil)
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        
+        startNewRound()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider){
         let currentValue = Int(slider.value)
-        sLabel.text = "\(currentValue)"
+        sLabel.text = "\(currentValue)" // displays the current number on the slider in real time
             }
     
     func startNewRound() {
         round += 1
         targetValue = Int.random(in: 1...100)
-        currentValue = 50
+        currentValue = 1 // moves the slider back to 1 after each round
         slider.value = Float(currentValue)
+        sLabel.text = "\(currentValue)"
     }
 }
